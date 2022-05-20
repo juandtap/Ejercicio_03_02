bddf53eef2c3fc65022d4c183b7fd5cb5c0a3fba
Paul Aguaiza <Paul Aguaiza@169.254.2.231>
2022-05-20 08:33:34 -0500

    VENTANAS TERMINADAS

--- src/main/java/ups/ejercicio_03_02/Ejercicio_03_02.java
+++ src/main/java/ups/ejercicio_03_02/Ejercicio_03_02.java
@@ -12,7 +12,13 @@ package ups.ejercicio_03_02;
 public class Ejercicio_03_02 {
 
     public static void main(String[] args) {
-        var v3 = new Ventana01(" INSCRIBETE");
-        v3.setVisible(true);
+        var v = new Ventana01(" INSCRIBETE");
+        v.setVisible(true);
+        
+        var v1 = new Ventana02("  INFORMACION DE CARRERA  ");
+        v1.setVisible(true);
+        
+        var v2 = new Ventana03("  DATOS PERSONALES  ");
+        v2.setVisible(true);
     }
 }
